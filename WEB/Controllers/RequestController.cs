f8a3501fda3b4be4840f91101a3013e2de3cb8f9
anonymmed <mohamed.abdelhafidh@esprit.tn>
2018-11-25 17:57:38 +0100

    updating project

--- WEB/Controllers/RequestController.cs
+++ WEB/Controllers/RequestController.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
+using WEB.Models;
 
 namespace WEB.Controllers
 {
+
     public class RequestController : Controller
     {
-        // GET: Request
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<RequestModel> requestList;
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:18080/map-web/map/User/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("getAllRequests").Result;
+
+                requestList = response.Content.ReadAsAsync<IEnumerable<RequestModel>>().Result;
+         
+            return View(requestList);
         }
     }
 }
