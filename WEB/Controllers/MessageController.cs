eacbd512a42c7616c8c06a85abb9e837da9f2976
anonymmed <mohamed.abdelhafidh@esprit.tn>
2018-11-26 23:31:46 +0100

    adding Message controller

---
+++ WEB/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEB.Models;
+
+namespace WEB.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult sendMessageToResource()
+        {
+            return View(new MessageModel());
+        }
+    }
+}
