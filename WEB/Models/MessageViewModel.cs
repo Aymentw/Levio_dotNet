c828327bdb32adf5a1f22cf8faa8990b999817b6
anonymmed <mohamed.abdelhafidh@esprit.tn>
2018-11-26 23:42:57 +0100

    adding messageViewModel

---
+++ WEB/Models/MessageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB.Models
+{
+    public class MessageViewModel
+    {
+        public IEnumerable<MessageModel> Messages { get; set; }
+        public IEnumerable<ConversationModel> Conversations { get; set; }
+
+    }
+}
